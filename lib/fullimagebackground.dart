4e01d876804be9c7e6de9fdf392651eab77598b4
deepakgupta <mcaneerajpanday@gmail.com>
2020-05-27 16:55:29 +0530

    full image as background sample

---
+++ lib/fullimagebackground.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: ShowFullImageSample(),
+    );
+  }
+}
+
+class ShowFullImageSample extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+          decoration: BoxDecoration(
+        image: DecorationImage(
+            image: AssetImage("assets/back.jpg"), fit: BoxFit.cover),
+      )),
+    );
+  }
+}
